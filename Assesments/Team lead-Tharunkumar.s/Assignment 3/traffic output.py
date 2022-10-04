91d00c47124bf73b844aafb22a71369540018b42
Tharun123997 <113616383+Tharun123997@users.noreply.github.com>
2022-10-04 19:48:19 +0530

    Add files via upload

---
+++ Assesments/Team lead-Tharunkumar.s/Assignment 3/traffic output.py
@@ -0,0 +1,22 @@
+Python 3.11.0a7 (main, Apr  5 2022, 21:27:39) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=============== RESTART: C:/Users/JAYARAJ/Desktop/Traffic code.py ==============
+Enter value: 1
+Green light
+
+=============== RESTART: C:/Users/JAYARAJ/Desktop/Traffic code.py ==============
+Enter value: 2
+Green light
+
+=============== RESTART: C:/Users/JAYARAJ/Desktop/Traffic code.py ==============
+Enter value: 3
+Green light
+
+=============== RESTART: C:/Users/JAYARAJ/Desktop/Traffic code.py ==============
+Enter value: 15
+Yellow light
+
+=============== RESTART: C:/Users/JAYARAJ/Desktop/Traffic code.py ==============
+Enter value: 25
+Red light
